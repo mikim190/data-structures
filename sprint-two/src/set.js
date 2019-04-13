6bec705ea323106b269a40ed2832ea7652861275
mikim190 <mykim.tran297@gmai.com>
2019-04-12 20:23:31 -0700

    Complete and pass all tests for set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  if (item in this._storage) {
+    return true;
+  }
+  return false;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  if (this.contains(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
