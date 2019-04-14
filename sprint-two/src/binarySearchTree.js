f840ebc4a899d80984dbc848c854d34e4bec3e87
mikim190 <mykim.tran297@gmai.com>
2019-04-13 18:18:24 -0700

    Complete BinarySearchTree. Working on HashTable, remove fn

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,71 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+  var nodeTree = {
+    value: value,
+    left: null,
+    right: null
+  };
+  _.extend(nodeTree, treeMethod);
+  return nodeTree;
+};
+
+var treeMethod = {
+  insert: function (value) {
+    var subTree = BinarySearchTree(value);
+    return (function findTreeLocation(loc) {
+      if (subTree.value < loc.value) {
+        if (loc.left === null) {
+          loc.left = subTree;
+          return;
+        }
+        if (loc.left !== null) {
+          return findTreeLocation(loc.left)
+        }
+      }
+      if (subTree.value > loc.value) {
+        if (loc.right === null) {
+          loc.right = subTree;
+          return;
+        }
+        if (loc.right !== null) {
+          return findTreeLocation(loc.right)
+        }
+      }
+    })(this)
+
+  },
+
+  contains: function (value) {
+    return (function hasTree(loc) {
+      if (loc === null) {
+        return false;
+      }
+      if (value === loc.value) {
+        return true;
+      } else if (value < loc.value) {
+        return hasTree(loc.left);
+      } else if (value > loc.value) {
+        return hasTree(loc.right);
+      } else {
+        return false;
+      }
+    })(this)
+  },
+
+  depthFirstLog: function (cb) {
+    cb(this.value)
+
+    if (this.left !== null) {
+      this.left.depthFirstLog(cb);
+    }
+    if (this.right !== null) {
+      this.right.depthFirstLog(cb);
+    }
+  }
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,51 @@
 
 
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
+
+
+
+
+
+
+
+  // for (var i = 0; i < this._storage.length; i++) {
+  //   if (_.storage[index] === undefined) {
+  //     this._storage.set(index, [k, v]);
+  //   } else {
+  //     for (var j = 0; j < this._storage[index][i]; j++) {
+  //       if (this._storage[index][i][0] === k) {
+  //         this._storage[index][i][0] = v;
+  //       }
+  //     }
+  //   }
+  // }
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index);
+
+
+
+
+
+
+
+
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+
+
 };
 
 
